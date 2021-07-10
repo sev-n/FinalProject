39fa9f91cecd610e771c44e73713d0ffe58ee66d
sev-n <74961058+sev-n@users.noreply.github.com>
2021-07-10 12:31:50 +0800

    Added

---
+++ src/aboutus.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+void about_us(){
+	system("COLOR 3");
+	
+	cout<<"\t\t================================================================================";
+    cout<<"\n\t\t\t\t\t\t*** ABOUT US ***\n";
+    cout<<"\n\t\t================================================================================\n\n";
+  
+    cout<<"\t\t\t\t\t\t  OBJECTIVES\n\n\n";
+    cout<<"\t\tThis programming project objective is to provide the student with the opportunity to"<<endl;
+    cout<<"\t   work on a computer program to solve a simple related problem. This program contains the C++"<<endl;
+    cout<<"\t   compiled basic solved programs. We, the programmers, provided the most important programs on"<<endl;
+    cout<<"\t   each topic. Every given programs includes the description, code, as well as the output of the"<<endl;
+    cout<<"\t   program. All given programs are compiled and tested on a windows system."<<endl;
+    
+    cout<<"\n\n\t\t\t\t\t\t[THE PROGRAMMERS]\n\n"<<endl;
+    cout<<"\t   LEADERS:\n"<<endl;
+    cout<<"\t\t[1] NAME: Denniel Lurion\t\t\t\t\t[2] NAME: Nerwin Panis"<<endl;
+    cout<<"\t\t    AGE : 19\t\t\t\t\t\t\t    AGE : 19"<<endl;
+    cout<<"\t\t    GNDR: Male\t\t\t\t\t\t\t    GNDR: Male"<<endl;
+    cout<<"\t\t    HBBS: Online Games & Self-Study(Programming)\t\t    HBBS: Basketball & Coding"<<endl;
+    cout<<"\t\t    ADRS: San Pedro laguna\t\t\t\t\t    ADRS: San Pedro Laguna\n\n"<<endl;
+    
+    cout<<"\t   MEMBERS:\n"<<endl;
+    cout<<"\t\t[1] NAME: John Henry Angas\t\t\t\t\t[2] NAME: Harold Son Enriquez"<<endl;
+    cout<<"\t\t    AGE : 19\t\t\t\t\t\t\t    AGE : 19"<<endl;
+    cout<<"\t\t    GNDR: Male\t\t\t\t\t\t\t    GNDR: Male"<<endl;
+    cout<<"\t\t    HBBS: Online Games & Play Musical Instrument\t\t    HBBS: Play Basketball & Online Games"<<endl;
+    cout<<"\t\t    ADRS: GMA Cavite\t\t\t\t\t            ADRS: Silang Cavite\n\n"<<endl;
+    cout<<"\t\t[3] NAME: Kyle L.F. Galanta\t\t\t\t\t[4] NAME: Kristine Joyce Beato"<<endl;
+    cout<<"\t\t    AGE : 20\t\t\t\t\t\t\t    AGE : 19"<<endl;
+    cout<<"\t\t    GNDR: Male\t\t\t\t\t\t\t    GNDR: Female"<<endl;
+    cout<<"\t\t    HBBS: Cycling & Play Online Games\t\t\t            HBBS: Online Games & Making\
+	\t\t\t\t\t\t\t\t\t\t\t\t          Tiktok Videos"<<endl;
+    cout<<"\t\t    ADRS: Paliparan III Cavite\t\t\t\t\t    ADRS: GMA Cavite\n\n"<<endl;
+    cout<<"\t\t[5] NAME: Melvin Vardeleon\t\t\t\t\t[6] NAME: Cedrick John Guevarra"<<endl;
+    cout<<"\t\t    AGE : 18\t\t\t\t\t\t\t    AGE : 19"<<endl;
+    cout<<"\t\t    GNDR: Male\t\t\t\t\t\t\t    GNDR: Male"<<endl;
+    cout<<"\t\t    HBBS: Online Games, Motorcycle\t\t\t\t    HBBS: Music & Play Online Games"<<endl;
+    cout<<"\t\t    ADRS: Paliparan I Cavite\t\t\t\t\t    ADRS: Sta.Rosa Laguna\n\n"<<endl;
+    cout<<"\t\t[7] NAME: Jomari De Guzman\t\t\t\t\t[8] NAME: Milbert Sigaan"<<endl;
+    cout<<"\t\t    AGE : 19\t\t\t\t\t\t\t    AGE : 19"<<endl;
+    cout<<"\t\t    GNDR: Male\t\t\t\t\t\t\t    GNDR: Male"<<endl;
+    cout<<"\t\t    HBBS: Music & Play Online Games\t\t\t\t    HBBS: Play Online Games"<<endl;
+    cout<<"\t\t    ADRS: Paliparan Cavite\t\t\t\t\t    ADRS: GMA Cavite\n\n"<<endl;
+	
+	cout<<"\t\t\t\t\t\t[DATA PRIVACY STATEMENT]\n\n"<<endl;
+	cout<<"\t\tOne of The Main Programmer (Nerwin Panis), collects a variety of information that the other programmers"<<endl; 
+	cout<<"\t   will provide directly to him. The members also have the option not to give their information if they want to.\n\n"<<endl;
+	
+	cout<<"\t\t\t\t\t\t[DATA PRIVACY AGREEMENT]\n\n"<<endl;
+	cout<<"\t\tWe, the members, have read Data Privacy Statement and express my consent for the main programmer"<<endl;
+	cout<<"\t   (Nerwin Panis) to Collect, Record, Organize, Update, or Modify, Retrieve, Consult, Use, Block, Erase, "<<endl;
+	cout<<"\t   Consolidate, Destruct my personal data as part of my information.\n"<<endl;
+	
+	cout<<"\t\tWe hereby, affirm our right to be informed, object to process, access and rectify, suspend or"<<endl;
+	cout<<"\t   withdraw my personal data, and be indemnified in case of damages pursuant to the provisions of the"<<endl;
+	cout<<"\t   Republic Act No. 10173 of the Philippines, Data Privacy of 2012 and its corresponding Implementing"<<endl;
+	cout<<"\t   Rules and Regulations\n\n"<<endl;
+    
+}
